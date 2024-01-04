a3ea41216cbadd2513ea38bce707d3b89ce714a5
Fudi <fudi.mahfudin@gmail.com>
2024-01-04 20:14:10 +0700

    Update Cart page

---
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { ChangeEvent, useState } from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { Media } from '@/src/app/_components/Media';
+import { Price } from '@/src/app/_components/Price';
+import { RemoveFromCartButton } from '@/src/app/_components/RemoveFromCartButton';
+
+import classes from './index.module.scss'
+
+const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
+  const [quantity, setQuantity] = useState(qty);
+
+  const decrementQty = () => {
+    const updatedQty = quantity > 1 ? quantity - 1 : 1;
+    setQuantity(updatedQty);
+    addItemToCart({ product, quantity: Number(updatedQty)});
+  }
+  const incrementQty = () => {
+    const updatedQty = quantity + 1;
+    setQuantity(updatedQty);
+    addItemToCart({ product, quantity: Number(updatedQty)});
+  }
+  const enterQty = (e: ChangeEvent<HTMLInputElement>) => {
+    const updatedQty = Number(e.target.value);
+    setQuantity(updatedQty);
+    addItemToCart({ product, quantity: Number(updatedQty)});
+  }
+
+  return (
+    <li className={classes.item}>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+
+        <div className={classes.quantity}>
+          <div className={classes.quantityBtn} onClick={decrementQty}>
+            <Image src="/assets/icons/minus.svg" alt="minus" width={24} height={24} className={classes.qtnBt} />
+          </div>
+          <input type="text" className={classes.quantityInput} value={quantity} onChange={enterQty} />
+          <div className={classes.quantityBtn} onClick={incrementQty}>
+            <Image src="/assets/icons/plus.svg" alt="plus" width={24} height={24} className={classes.qtnBt} />
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
+        <RemoveFromCartButton product={product} />
+      </div>
+    </li>
+  )
+}
+
+export default CartItem
